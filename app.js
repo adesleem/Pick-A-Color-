96e00642a19062b71f73c181691545e9e6247a8c
Oloyede Adeosun <adesleem01@gmail.com>
2021-02-12 01:32:29 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let element1 = ('Apple', 'Orange' 100);
+let element2 = ('Table', 'Laptop' 105);
+let element3 = ('Plantain', 'Phone' 120);
+
+let table = [table.push(element1, element2, element3);]
+
+console.log(table)
